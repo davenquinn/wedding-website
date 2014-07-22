331e388be2e1afdf21bcd5b7ee9d3acc110651de
Daven Quinn <dev@davenquinn.com>
2014-07-22 15:19:08 -0700

    Added the ability of wedding section to store files.

--- __init__.py
+++ __init__.py
@@ -1,7 +1,21 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, current_app, flash
+from pathlib import Path
+from datetime import datetime
 from .forms import RSVPForm
 
 wedding = Blueprint('wedding', __name__, template_folder='templates')
+
+def complete_rsvp(app, form):
+    time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    fn = "{0} {1}.txt".format(form["name"],time)
+    p = Path(app.config["STORAGE_BASE"])/"wedding-responses"/fn
+    with p.open("w") as f:
+        w = lambda s: print(s,file=f)
+        w("Name: "+form["name"])
+        w("Email:"+form["email"])
+        w("No. attending: "+str(form["number"]))
+        w("Message:")
+        w(form["message"])
 
 @wedding.route('/')
 def index():
@@ -16,7 +30,13 @@ def rsvp():
     form = RSVPForm(request.form)
     if request.method == "POST":
         if form.validate():
-            return "<p class='success'>Congratulations, you've successfully RSVPd</p>"
-        else:
-            return render_template('wedding/rsvp_form.html', form=form)
-    return render_template('wedding/rsvp.html', form=form)
+            try:
+                complete_rsvp(current_app, request.form)
+            except Exception as err:
+                flash("There was an error submitting the form: "+str(err), "error")
+            else:
+                flash("Congratulations, you've successfully RSVPd","success")
+                return render_template('wedding/rsvp_form.html')
+        return render_template('wedding/rsvp_form.html', form=form)
+    else:
+        return render_template('wedding/rsvp.html', form=form)
